4cd678ae67cf1826b6579f68309d2d02d534f54e
Tfia731 <tfia731@gmail.com>
2023-10-02 08:52:20 -0400

    edit

--- block24a/index.js
+++ block24a/index.js
@@ -2,22 +2,22 @@
 // Is a programming paradigm that emphasizes the use of pure functions and immutable data.
 //FIRST CLASS FUNCTION TAB - 3 PROBLEM STATEMENT
 //1.Assign a function to a variable and use it to convert a string to uppercase:
-const convertToUpperCase = function (str) {
-    return str.toUpperCase();
-  };
-  const inputString = "Welcome";
-  const expected = convertToUpperCase(inputString);
-  console.log(expected);
+
+const assigningFunction = function (str) {
+  return str.toUpperCase();
+};
+console.log(assigningFunction("Welcome"));
+
   //2.Create a function that takes a number as input and returns another function that multiplies its input with the original number:
-  function createMultiplier(factor) {
-    return function (number) {
-      return factor * number;
+ 
+  function multiplierDisplay(a) {
+    return function (b) {
+      return a * b;
     };
   }
-  const multiplyBy3 = createMultiplier(3);
-  const inputNumber = 7;
-  const multi = multiplyBy3(inputNumber);
-  console.log(multi);
+  const double = multiplierDisplay(3);
+  console.log(double(7));
+
   //3.Create a function that takes two numbers as input and returns an object with specific properties:
   function createFunction() {
       return function (num1, num2) {
@@ -35,14 +35,96 @@ const convertToUpperCase = function (str) {
   console.log(result);
 
   // Higher- Order Function
-
-  //Write a higher-order function that takes an array and a modifying function as arguments. The higher-order function should modify each element in the input and return a new array containing the modified elements. 
-
+  
+  //Write a higher-order function that takes an array and a modifying function as arguments. The higher-order function should modify each element in the input and return a new array containing the modified elements.
   //1. Converts all string elements to lowercase
 
-  const convertToLowerCase = function (str) {
-    return str.toLowerCase();
+function higherOrderFunc(arr, modifyFunc) {
+  const modifiedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    modifiedArray.push(modifyFunc(arr[i]));
+  }
+  return modifiedArray;
+}
+const modifiedArray = higherOrderFunc(
+  ["Hippopotamus", "King Cobra", "Giant Panda", "Crocodile"],
+  function (str_modified) {
+    return str_modified.toLowerCase();
+  }
+);
+console.log(modifiedArray);
+
+//2. String length More than 11
+
+function filterStringChar(stringGiven, condition) {
+  return stringGiven.filter(condition);
+}
+
+function hasMoreThanElevenChars(string) {
+  return string.length > 11;
+}
+
+const stringArray = ["Hippopotamus", "King Cobra", "Giant Panda", "Crocodile"];
+
+const filteredStringArray = filterStringChar(
+  stringArray,
+  hasMoreThanElevenChars
+);
+
+console.log(filteredStringArray);
+
+
+//3. Reverse String
+
+function modifyGivenStrings(stringArray, callbackFunction) {
+  return stringArray.map(callbackFunction);
+}
+
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+const stringArray = ["Hippopotamus", "King Cobra", "Giant Panda", "Crocodile"];
+
+const modifiedTheStrings = modifyGivenStrings(stringArray, reverseString);
+
+console.log(modifiedTheStrings);
+
+//Runner 
+
+const createRunner = (speed) => (name) => {
+  return {
+    name,
+    speed,
+    location: 0,
+    move: function () {
+      this.location += speed;
+    },
   };
-  const inputString = ["Hippopotamus", "King Cobra", "Giant Panda", "Crocodile"];
-  const expected = convertToLowerCase(inputString);
-  console.log(expected);
+};
+
+const createFastRunner = createRunner(7);
+const createSlowRunner = createRunner(3);
+
+const foo = createFastRunner("FOO");
+const bar = createSlowRunner("BAR");
+
+function race(runner1, runner2, raceDistance) {
+  runner1.location = 0;
+  runner2.location = 0;
+
+  while (runner1.location < raceDistance && runner2.location < raceDistance) {
+    runner1.move();
+    runner2.move();
+  }
+
+  if (runner1.location >= raceDistance && runner2.location >= raceDistance) {
+    return "It's a tie!";
+  } else if (runner1.location >= raceDistance) {
+    return `${runner1.name} wins!`;
+  } else {
+    return `${runner2.name} wins!`;
+  }
+}
+
+console.log(race(foo, bar, 100));
